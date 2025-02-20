29fc8d28a34cdedbf4bedc977e1c18159504d21f
Santiago Montoya <santmordz@MacBook-Air-de-Santiago.local>
2025-02-20 17:55:51 -0500

    styles in basic form using Tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,26 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
-      <button className="bg-blue-500 text-white p-3.5 rounded-lg w-24 mx-auto block mb-5">
-        Click me
-      </button>
-     
-      <h1 className="text-violet-500 bg-[#50b23e]">Vite + React</h1>
-      
+      <div className="text-6xl font-extrabold text-center my-8">
+        <span className="bg-clip-text text-transparent bg-gradient-to-r from-pink-600 to-violet-500 ">
+          Hello world!
+        </span>
+      </div>
 
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <form className="bg-white w-96 mx-auto rounded-lg p-10">
+        <input className="border border-gray-50 w-full px-3 py-2 mb-4 rounded-md disabled:bg-gray-400" disabled type="text" placeholder="Enter your name" />
+
+        <input className="border border-gray-300 w-full px-3 py-2 mb-4 rounded-md focus:outline-none focus:ring-1 focus:ring-amber-200 invalid:focus:ring-red-200 peer" type="email" placeholder="Enter your email" />
+
+        <p className="ms-2 mt-0 mb-4 text-red-500 hidden peer-invalid:block">El correo es incorrecto</p>
+
+        <input className="border border-gray-300 w-full px-3 py-2 mb-8 rounded-md focus:outline-none" type="password" placeholder="Enter your password" />
+
+        <input className="rounded-full bg-blue-500 w-full text-white py-2 cursor-pointer hover:bg-blue-400 " type="submit" value="Submit" />
+      </form>
+
     </>
   );
 }
