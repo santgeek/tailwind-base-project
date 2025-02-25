2ff869890eae651ffe94927ba948f617fdfe9c8e
Santiago Montoya <santmordz@MacBook-Air-de-Santiago.local>
2025-02-25 16:55:30 -0500

    styles test

--- src/App.jsx
+++ src/App.jsx
@@ -40,12 +40,78 @@ function App() {
         />
       </form>
 
-      <div className="mt-8 border-2 border-black p-6 w-80 mx-auto">
-        <h2 className="text-xl font-bold">Coding Tube</h2>
-        <p className="mt-4">React, TailwindCSS y Vite las herramientas que necesitas para crear tu aplicación web</p>
+      <div className="mt-8 mb-8 flex place-content-around border-2 border-black h-[600px] flex-wrap gap-4">
+        <div className="card shrink-0">1</div>
+        <div className="card shrink-0">2</div>
+        <div className="card shrink-0">3</div>
+        <div className="card shrink-0">1</div>
+        <div className="card shrink-0">2</div>
+        <div className="card grow">3</div>
       </div>
 
-      
+      <div className="mt-8 flex border-2 border-black h-[600px] gap-4">
+        <div className="card shrink-0 basis-1/4 sm:basis-1/3">1</div>
+        <div className="card shrink-0 basis-1/4 sm:basis-1/3">2</div>
+        <div className="card shrink-0 basis-1/2 sm:basis-1/3">3</div>
+        
+      </div>
+
+      <div className="mt-8 grid grid-cols-4 gap-4">
+        <div className="card col-span-2">1</div>
+        <div className="card ">1</div>
+        <div className="card ">2</div>
+        <div className="card row-span-2">3</div>
+        <div className="card ">4</div>
+        <div className="card row-span-2 col-span-2">5</div>
+        <div className="card">6</div>
+        <div className="card col-span-3">7</div>
+        <div className="card ">8</div>
+        <div className="card ">9</div>
+      </div>
+
+      <div className="mt-8 grid grid-cols-2 gap-4 h-[100vh]">
+        <div className="card col-span-2">Header</div>
+        <div className="card col-span-2">Nav</div>
+        <div className="card ">section</div>
+        <div className="card row-span-2">article</div>
+        <div className="card row-[3/6] col-start-2">aside</div>
+        <div className="card col-span-2">footer</div>
+        
+      </div>
+
+      <section className="mb-8 xl:flex">
+        <div className="mt-8 border-2 border-black p-6 w-80 mx-auto">
+          <h2 className="text-xl font-bold lg:text-3xl sm:text-2xl">
+            Coding Tube
+          </h2>
+          <p className="mt-4">
+            React, TailwindCSS y Vite las herramientas que necesitas para crear
+            tu aplicación web
+          </p>
+        </div>
+
+        <div className="mt-8 border-2 border-black p-6 w-80 mx-auto">
+          <h2 className="text-xl font-bold lg:text-3xl sm:text-2xl">
+            Coding Tube
+          </h2>
+          <p className="mt-4">
+            React, TailwindCSS y Vite las herramientas que necesitas para crear
+            tu aplicación web
+          </p>
+        </div>
+
+        <div className="mt-8 border-2 border-black p-6 w-80 mx-auto">
+          <h2 className="text-xl font-bold lg:text-3xl sm:text-2xl">
+            Coding Tube
+          </h2>
+          <p className="mt-4">
+            React, TailwindCSS y Vite las herramientas que necesitas para crear
+            tu aplicación web
+          </p>
+        </div>
+      </section>
+
+
 
     </div>
   );
